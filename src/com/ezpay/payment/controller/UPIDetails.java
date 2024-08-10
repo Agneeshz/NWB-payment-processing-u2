0c5b7a68c60538dac845256043573f3e50ec4ab7
adithyamode <123758039+adithyamode@users.noreply.github.com>
2024-08-10 16:52:17 +0530

    Update UPIDetails.java

--- src/com/ezpay/payment/controller/UPIDetails.java
+++ src/com/ezpay/payment/controller/UPIDetails.java
@@ -1,1 +1,37 @@
+package com.ezpay.payment.controller;
+import java.util.Scanner;
+import com.ezpay.payment.service.UPIService;
+
+public class UPIDetails 
+{
+    static Scanner scanner = new Scanner(System.in);
+
+    static public void getUPIDetails()
+    {
+        System.out.println("Please enter your UPI ID : ");
+        String upiId = scanner.nextLine();
+
+        System.out.println("Please enter the amount : ");
+        int amount = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Please confirm your details");
+        System.out.println("UPI ID : " + upiId);
+        System.out.println("Amount : " + amount);
+        System.out.println("Enter YES to initiate the transaction");
+        String confirmation = scanner.nextLine();
+
+        if(confirmation.equalsIgnoreCase("YES"))
+        {
+            System.out.println("Please wait. Your transaction is in progress");
+            UPIService.verifyDetails(upiId, amount);
+        }
+            
+        else
+        {
+            System.out.println("Transaction is aborted");
+        }
+
+    }
+}
 
