d5246fa5a5adcb74a3e69d03112984ba0a65b1bc
adithyamode <123758039+adithyamode@users.noreply.github.com>
2024-08-18 01:29:19 +0530

    Update and rename UPIRepository.java to BankUserRepository.java

---
+++ src/com/ezpay/payment/repository/BankUserRepository.java
@@ -0,0 +1,25 @@
+package Repo;
+
+import Model.BankUser;
+import java.util.HashMap;
+import java.util.Map;
+
+public class BankUserRepository {
+
+    private Map<String, BankUser> bankusers = new HashMap<>();
+
+    public BankUserRepository() {
+        // Initializing with some sample users
+        bankusers.put("99887744556", new BankUser("Adithya Mode", "99887744556", "SBIN096321",  5000));
+        bankusers.put("88997755664", new BankUser("Agneesh Dasgupta", "88997755664", "SBIN098125" ,8000));
+        bankusers.put("88995566447", new BankUser("Deepak Reddy", "88995566447", "SBIN096325" ,10000));
+        bankusers.put("88662211335", new BankUser("Aishveen Kaur", "88662211335","SBIN098512" , 12000));
+        bankusers.put("88997755661", new BankUser("Hasini", "88997755661","SBIN098745" , 15000));
+    }
+
+    public BankUser findUserByAccountNumber(String accountNumber) {
+        return bankusers.get(accountNumber);
+    }
+
+    
+}
