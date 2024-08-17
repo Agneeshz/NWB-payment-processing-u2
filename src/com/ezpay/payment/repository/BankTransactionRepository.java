66f2185bef70b7099faf5712d10f4fe904a5efb9
adithyamode <123758039+adithyamode@users.noreply.github.com>
2024-08-18 01:28:50 +0530

    Update and rename BankAccountRepository.java to BankTransactionRepository.java

---
+++ src/com/ezpay/payment/repository/BankTransactionRepository.java
@@ -0,0 +1,19 @@
+package Repo;
+
+import Model.BankTransaction;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BankTransactionRepository {
+
+    private List<BankTransaction> bankTransactions = new ArrayList<>();
+
+    public void saveTransaction(BankTransaction transaction) {
+        bankTransactions.add(transaction);
+    }
+
+    public List<BankTransaction> getAllTransactions() {
+        return bankTransactions;
+    }
+
+}
