9c8d3b53be24e5ce94a16e0c449123b009f603c1
adithyamode <123758039+adithyamode@users.noreply.github.com>
2024-08-18 01:24:36 +0530

    Update PaymentController.java

--- src/com/ezpay/payment/controller/PaymentController.java
+++ src/com/ezpay/payment/controller/PaymentController.java
@@ -1,39 +1,30 @@
-/**
- * Module Name: PaymentController
- * 
- * Description: This module acts as the entry point for processing payments. It prompts the user to select a 
- * mode of payment (UPI or Bank Transfer) and directs the flow to the respective payment detail collection module.
- * 
- * Authors:
- * Agneesh Dasgputa
- * 
- * Date: August 10,2024
- */
+package Controller;
 
-package com.ezpay.payment.controller;
 import java.util.Scanner;
 
-public class PaymentController 
-{
-    //Assuming user has already logged in
-    static Scanner scanner = new Scanner(System.in);
+public class PaymentController {
 
-    public static void main(String[] args) 
-    {
+    public static void main(String[] args) {
+
+        //Assuming user has already logged in
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Please enter the mode of payment");
         System.out.println("UPI or Bank Transfer ?");
         String modeOfPayment = scanner.nextLine();
 
-
         if(modeOfPayment.equalsIgnoreCase("UPI"))
         {
-            UPIDetails.getUPIDetails();
+            UPIPaymentController upiPaymentController = new UPIPaymentController();
+            upiPaymentController.UPIDetails();
         }
 
         else if(modeOfPayment.equalsIgnoreCase("Bank Transfer"))
         {
-        	BankAccountDetails.getBankAccountDetails();
+        	BankTransferPaymentController bankPaymentController = new BankTransferPaymentController();
+            bankPaymentController.bankDetails();
         }
-		
-     }
+
+        scanner.close();
+    }
 }
