1a5e0b9f4f9a665c98f880a76ae98d5bf7179cb2
adithyamode <123758039+adithyamode@users.noreply.github.com>
2024-08-22 22:15:33 +0530

    Update UPITransaction.java

--- src/com/ezpay/payment/model/UPITransaction.java
+++ src/com/ezpay/payment/model/UPITransaction.java
@@ -10,55 +10,6 @@ public class UPITransaction {
     private String note;
     private String status;
 
-	
-    	public String getSenderUpiId() {
-		return senderUpiId;
-	}
-
-	public void setSenderUpiId(String senderUpiId) {
-		this.senderUpiId = senderUpiId;
-	}
-
-	public String getReceiverUpiId() {
-		return receiverUpiId;
-	}
-
-	public void setReceiverUpiId(String receiverUpiId) {
-		this.receiverUpiId = receiverUpiId;
-	}
-
-	public double getAmount() {
-		return amount;
-	}
-
-	public void setAmount(double amount) {
-		this.amount = amount;
-	}
-
-	public Date getDate() {
-		return date;
-	}
-
-	public void setDate(Date date) {
-		this.date = date;
-	}
-
-	public String getNote() {
-		return note;
-	}
-
-	public void setNote(String note) {
-		this.note = note;
-	}
-
-	public String getStatus() {
-		return status;
-	}
-
-	public void setStatus(String status) {
-		this.status = status;
-	}
-
     public UPITransaction(String senderUpiId, String receiverUpiId, double amount, Date date, String note, String status) {
         this.senderUpiId = senderUpiId;
         this.receiverUpiId = receiverUpiId;
@@ -66,6 +17,54 @@ public class UPITransaction {
         this.date = date;
         this.note = note;
         this.status = status;
+    }
+	
+    public String getSenderUpiId() {
+	return senderUpiId;
+    }
+
+    public void setSenderUpiId(String senderUpiId) {
+	this.senderUpiId = senderUpiId;
+    }
+
+    public String getReceiverUpiId() {
+	return receiverUpiId;
+    }
+
+    public void setReceiverUpiId(String receiverUpiId) {
+	this.receiverUpiId = receiverUpiId;
+    }
+
+    public double getAmount() {
+	return amount;
+    }
+
+    public void setAmount(double amount) {
+	this.amount = amount;
+    }
+
+    public Date getDate() {
+	return date;
+    }
+
+    public void setDate(Date date) {
+	this.date = date;
+    }
+
+    public String getNote() {
+	return note;
+    }
+
+    public void setNote(String note) {
+	this.note = note;
+    }
+
+    public String getStatus() {
+	    return status;
+    }
+
+    public void setStatus(String status) {
+	this.status = status;
     }
 
     // Getters and toString method
