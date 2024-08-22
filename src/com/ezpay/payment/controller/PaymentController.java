9b2965eafebe3b6454df79bc920218b8d52ec0d6
DEEPAK REDDY BIJIVEMULA <bijivemuladeepakreddy@gmail.com>
2024-08-22 11:10:37 +0530

    modified paymentController

--- src/com/ezpay/payment/controller/PaymentController.java
+++ src/com/ezpay/payment/controller/PaymentController.java
@@ -1,39 +1,43 @@
-/**
- * Module Name: PaymentController
- * 
- * Description: This module acts as the entry point for processing payments. It prompts the user to select a 
- * mode of payment (UPI or Bank Transfer) and directs the flow to the respective payment detail collection module.
- * 
- * Authors:
- * Agneesh Dasgputa
- * 
- * Date: August 10,2024
- */
-
 package com.ezpay.payment.controller;
+
 import java.util.Scanner;
 
-public class PaymentController 
-{
-    //Assuming user has already logged in
-    static Scanner scanner = new Scanner(System.in);
+public class PaymentController {
 
-    public static void main(String[] args) 
-    {
-        System.out.println("Please enter the mode of payment");
-        System.out.println("UPI or Bank Transfer ?");
-        String modeOfPayment = scanner.nextLine();
+    public static void main(String[] args) {
+        //Assuming user has already logged in
+        Scanner scanner = new Scanner(System.in);
+        boolean flag = true;
+        while(flag) {
+        System.out.println("Please enter the mode of payment:");
+        System.out.println("1. UPI");
+        System.out.println("2. Bank Transfer");
+        System.out.println("3. Exit");
+        System.out.print("Enter your choice: ");
 
-
-        if(modeOfPayment.equalsIgnoreCase("UPI"))
-        {
-            UPIDetails.getUPIDetails();
+        int choice = scanner.nextInt(); // Read the user choice as an integer
+        scanner.nextLine(); // Consume the newline character left after nextInt()
+        
+        switch (choice) {
+            case 1:
+                UPIPaymentController upiPaymentController = new UPIPaymentController();
+                upiPaymentController.UPIDetails();
+                flag = false;
+                break;
+            case 2:
+                BankTransferPaymentController bankPaymentController = new BankTransferPaymentController();
+                bankPaymentController.bankDetails();
+                flag = false;
+                break;
+            case 3:
+                System.out.println("Exiting...");
+                flag = false;
+                break;
+            default:
+                System.out.println("Invalid input. Please enter a valid option!");
+                break;
         }
-
-        else if(modeOfPayment.equalsIgnoreCase("Bank Transfer"))
-        {
-        	BankAccountDetails.getBankAccountDetails();
         }
-		
-     }
+        scanner.close();
+    }
 }
