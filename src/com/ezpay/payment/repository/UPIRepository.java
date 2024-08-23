8e325caecd01b5ff92ac0fdb06abb46bad1cf204
adithyamode <123758039+adithyamode@users.noreply.github.com>
2024-08-23 10:03:28 +0530

    Update UPIRepository.java

--- src/com/ezpay/payment/repository/UPIRepository.java
+++ src/com/ezpay/payment/repository/UPIRepository.java
@@ -37,14 +37,14 @@ public class UPIRepository {
      */
     public UPI findUserByUpiId(String upiId) {
         String query = "SELECT * FROM upi WHERE upi_id = ?";
-        try (PreparedStatement statement = connection.prepareStatement(query)) {
-            statement.setString(1, upiId);  // Set the UPI ID parameter in the query
-            ResultSet result = statement.executeQuery();  // Execute the query
-            if (result.next()) {
-                String custName = result.getString("cust_name");  // Retrieve customer name
-                double balance = result.getDouble("balance");  // Retrieve balance
-                long mobileNumber = result.getLong("mobile_number");  // Retrieve mobile number
-                String email = result.getString("email");  // Retrieve email
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setString(1, upiId);  // Set the UPI ID parameter in the query
+            ResultSet resultSet = preparedStatement.executeQuery();  // Execute the query
+            if (resultSet.next()) {
+                String custName = resultSet.getString("cust_name");  // Retrieve customer name
+                double balance = resultSet.getDouble("balance");  // Retrieve balance
+                long mobileNumber = resultSet.getLong("mobile_number");  // Retrieve mobile number
+                String email = resultSet.getString("email");  // Retrieve email
                 return new UPI(custName, upiId, balance, mobileNumber, email);  // Return a UPI object with the retrieved details
             }
         } catch (SQLException e) {
@@ -61,10 +61,10 @@ public class UPIRepository {
     public void updateUser(UPI upi) {
         try (Connection conn = DBConnection.getConnection()) {  // Get a new database connection
             String query = "UPDATE upi SET balance = ? WHERE upi_id = ?";
-            PreparedStatement statement = conn.prepareStatement(query);
-            statement.setDouble(1, upi.getBalance());  // Set the updated balance
-            statement.setString(2, upi.getUpiId());  // Set the UPI ID
-            statement.executeUpdate();  // Execute the update query
+            PreparedStatement preparedStatement = conn.prepareStatement(query);
+            preparedStatement.setDouble(1, upi.getBalance());  // Set the updated balance
+            preparedStatement.setString(2, upi.getUpiId());  // Set the UPI ID
+            preparedStatement.executeUpdate();  // Execute the update query
         } catch (SQLException e) {
             e.printStackTrace();  // Print stack trace if an SQL exception occurs
         }
