d33f0697d3f80c6d536d80e64a5ddc578972d296
adithyamode <123758039+adithyamode@users.noreply.github.com>
2024-08-23 19:42:16 +0530

    Update UPITransactionRepository.java

--- src/com/ezpay/payment/repository/UPITransactionRepository.java
+++ src/com/ezpay/payment/repository/UPITransactionRepository.java
@@ -1,15 +1,20 @@
 /**
+ * Module Name: UPITransactionRepository
+ *
+ * Description: This module defines the repository interface for handling UPI transactions in the payment processing system.
+ * It provides methods for saving a UPI transaction and retrieving transactions based on the UPI ID of the sender or receiver.
+ * The interface includes SQL queries for inserting and finding UPI transactions in the database.
  * 
+ * Author:
+ * Agneesh Dasgupta
+ * 
+ * Date: August 23, 2024
  */
+
 package com.ezpay.payment.repository;
-
 import java.util.List;
-
 import com.ezpay.payment.model.UPITransaction;
 
-/**
- * @author Agneesh Dasgupta
- */
 public interface UPITransactionRepository {
 	void saveTransaction(UPITransaction transaction);
 	String QUERY_INSERT_UPITRANSACTIONS = "INSERT INTO upi_transactions (sender_upi_id, receiver_upi_id, amount, transaction_date, note, status) VALUES (?, ?, ?, ?, ?, ?)";
